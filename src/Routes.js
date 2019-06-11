c77874d20c3208e58978b5e518316ad03ebbdd43
chengyu <chengyu0316@gmail.com>
2019-06-11 22:51:30 +1000

    add settings page

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import Home from "./containers/Home";
 import Notes from "./containers/Notes";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import Settings from "./containers/Settings";
 import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
@@ -15,8 +16,10 @@ export default ({ childProps }) =>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
     <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute path="/settings" exact component={Settings} props={childProps} />
     <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
-  </Switch>;
+  </Switch>
+
