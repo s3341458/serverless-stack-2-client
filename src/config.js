b1c733ca92815955fe6ce47682a57815b7d6e0a2
chengyu <chengyu0316@gmail.com>
2019-06-11 22:27:32 +1000

    config enviroment

--- src/config.js
+++ src/config.js
@@ -1,17 +1,45 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-2-api-dev-serverlessdeploymentbucket-6lw5huob381q"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "ap-southeast-2",
+    URL: "https://api-dev.theyucheng.com"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_NkaaHXyIA",
+    APP_CLIENT_ID: "7kgmb32lec7brt18pmv7cacdko",
+    IDENTITY_POOL_ID: "ap-southeast-2:d022a4aa-f427-413c-aecf-3552dbeb2765"
   }
 };
+
+const prod = {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-2-api-dev-serverlessdeploymentbucket-6lw5huob381q"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://api.theyucheng.com"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_NkaaHXyIA",
+    APP_CLIENT_ID: "7kgmb32lec7brt18pmv7cacdko",
+    IDENTITY_POOL_ID: "ap-southeast-2:d022a4aa-f427-413c-aecf-3552dbeb2765"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
+};
+
